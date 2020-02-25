46f65380f50e27ecb6f080d8c162d4d1558c6dcb
Martin Keiblinger <zombie@martins-mini.home>
2020-02-25 23:00:49 +0100

    feat(): Initial commit.

---
+++ SimpleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
